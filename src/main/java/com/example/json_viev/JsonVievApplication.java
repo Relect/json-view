112357734f2ec950ab97eee2181e459885f6460f
Gennadiy <relect@bk.ru>
2024-11-28 14:33:21 +0300

    first commit

---
+++ src/main/java/com/example/json_viev/JsonVievApplication.java
@@ -0,0 +1,13 @@
+package com.example.json_viev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonVievApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonVievApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/json_viev/JsonVievApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.json_viev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonVievApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
